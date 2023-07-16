4a82969f86b8a60e33cb63cd41d1c25288392b2a
Frans <frans_421_12@hotmail.com>
2023-07-15 23:30:21 -0500

    feat: Listar empleados

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,15 +1,38 @@
 <template>
     <div>
-
+        <div v-for="employee in employees" :key="employee.id">
+            <div>{{ employee.id }}</div>
+            <div>{{ employee.name }}</div>
+            <div>{{ employee.email }}</div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
+    data () {
         
 
-        return {}
+        return {
+            employees:[]
+        }
+    },
+    created:function(){
+        this.consultEmployees();
+    },
+    methods:{
+        consultEmployees(){
+            fetch('http://localhost/employees/')
+            .then(response => response.json())
+            .then((dataResponse) => {
+                console.log(dataResponse);
+                this.employees = []
+                if (typeof dataResponse[0].success === 'undefined') {
+                    this.employees = dataResponse;
+                }
+            })
+            .catch(console.log)
+        }
     }
 }
 </script>
