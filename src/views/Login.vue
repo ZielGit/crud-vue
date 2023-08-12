afdfaee6a9a28122982618773cfc4b4c03e68982
Frans <frans_421_12@hotmail.com>
2023-08-11 23:27:05 -0500

    feat: login sanctum

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,45 @@
 <template>
     <div class="container mt-4">
-        <pre>{{ user }}</pre>
-        <form @submit.prevent="login">
-            <input v-model="form.email" type="email" placeholder="Enter your email">
-            <input v-model="form.password" type="password" placeholder="************">
-            <button>Login</button>
-        </form>
+        <div class="card">
+            <div class="card-body">
+                <pre>{{ user }}</pre>
+                <form @submit.prevent="login">
+                    <div class="mb-3">
+                      <label for="email" class="form-label">Email</label>
+                      <input v-model="form.email" type="email" class="form-control" id="email" placeholder="Enter your email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input v-model="form.password" type="password" class="form-control" id="password" placeholder="************">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000';
 
 export default {
     data: () => ({
         user: {},
         form: {
-            email: '',
-            password: ''
+            email: 'frans@gmail.com',
+            password: 'password'
         }
     }),
     methods: {
         login() {
-            axios.post("http://localhost:8080/login").then(res => {
-                console.log(res);
+            axios.get('/sanctum/csrf-cookie').then(() => {
+                axios.post("/login").then(res => {
+                    console.log(res);
+                });
             });
+            
             console.log("Enviando el login...", this.form);
         }
     }
