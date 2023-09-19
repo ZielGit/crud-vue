118b8f40aafce68b57c971ea4c23a5dbaf03aedb
Frans <frans_421_12@hotmail.com>
2023-09-18 22:45:43 -0500

    feat: Composition API

--- src/views/Customer/Create.vue
+++ src/views/Customer/Create.vue
@@ -3,17 +3,34 @@
         <div class="card">
             <div class="card-header">Create Customer</div>
             <div class="card-body">
-                <form v-on:submit.prevent="addRegister">
-                    <div class="mb-3">
-                      <label for="name" class="form-label">Nombre</label>
-                      <input type="text" class="form-control" name="" id="name" v-model="customer.name" placeholder="" required>
-                    </div>
-                    <div class="mb-3">
-                      <label for="email" class="form-label">Correo</label>
-                      <input type="email" class="form-control" name="" id="email" v-model="customer.email" placeholder="" required>
+                <form v-on:submit.prevent="saveCustomer">
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="document_type">Tipo de Documento</label>
+                            <select class="form-control" name="" id="document_type" v-model="customer.document_type">
+                                <option value="0" selected>DNI</option>
+                                <option value="1">RUC</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="document_number">Nr de Documento</label>
+                            <input type="number" class="form-control" name="" id="document_number" v-model="customer.document_number" min="0" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="name" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" name="" id="name" v-model="customer.name" placeholder="" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="last_name" class="form-label">Apellido</label>
+                            <input type="text" class="form-control" name="" id="last_name" v-model="customer.last_name" placeholder="" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="email" class="form-label">Correo</label>
+                            <input type="email" class="form-control" name="" id="email" v-model="customer.email" placeholder="" required>
+                        </div>
                     </div>
                     <div class="btn-group" role="group" aria-label="Button group name">
-                        <button type="submit" class="btn btn-primary">Agregar</button>
+                        <button type="submit" class="btn btn-primary me-2">Agregar</button>
                         <router-link :to="{name:'customers.index'}" class="btn btn-secondary">Cancelar</router-link>
                     </div>
                 </form>
@@ -22,36 +39,32 @@
     </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            customer:{}
-        }
-    },
-    methods:{
-        addRegister(){
-            var data = {
-                name: this.customer.name,
-                email: this.customer.email
-            }
-            console.log(data);
-            fetch('http://localhost:8000/api/customers',{
-                method:"POST",
-                body: JSON.stringify(data),
-                headers: { "Content-Type": "application/json" }
-            })
-            .then(response => response.json())
-            .then((dataResponse) => {
-                console.log(dataResponse);
-                window.location.href='/customers'
-            })
-            .catch(console.log)
-        }
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import router from '@/router';
+import { showToastSuccess, handleErrors } from '@/utils/index';
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+var customer = ref({
+    document_type: '',
+    document_number: '',
+    name: '',
+    last_name: '',
+    email: ''
+});
+
+const saveCustomer = async () => {
+    try {
+        customer.value.document_number = String(customer.value.document_number);
+        const response = await axios.post(`${apiUrl}/customers`, customer.value);
+        router.push('/customers');
+        showToastSuccess(response.data.message);
+        console.log(customer.value);
+    } catch (error) {
+        handleErrors(error);
+        console.log(customer.value);
     }
-}
+};
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- src/views/Customer/Edit.vue
+++ src/views/Customer/Edit.vue
@@ -3,14 +3,31 @@
         <div class="card">
             <div class="card-header">Edit Customer</div>
             <div class="card-body">
-                <form v-on:submit.prevent="editRegister">
-                    <div class="mb-3">
-                      <label for="name" class="form-label">Name</label>
-                      <input type="text" class="form-control" id="name" v-model="customer.name" placeholder="" required>
-                    </div>
-                    <div class="mb-3">
-                      <label for="email" class="form-label">Email</label>
-                      <input type="email" class="form-control" id="email" v-model="customer.email" placeholder="" required>
+                <form v-on:submit.prevent="updateCustomer">
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="document_type">Tipo de Documento</label>
+                            <select class="form-control" name="" id="document_type" v-model="formData.document_type">
+                                <option value="0">DNI</option>
+                                <option value="1">RUC</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="document_number">Nr de Documento</label>
+                            <input type="number" class="form-control" name="" id="document_number" v-model="formData.document_number" min="0" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="name" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" name="" id="name" v-model="formData.name" placeholder="" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="last_name" class="form-label">Apellido</label>
+                            <input type="text" class="form-control" name="" id="last_name" v-model="formData.last_name" placeholder="" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="email" class="form-label">Correo</label>
+                            <input type="email" class="form-control" name="" id="email" v-model="formData.email" placeholder="" required>
+                        </div>
                     </div>
                     <div>
                         <button type="submit" class="btn btn-primary me-1">Edit</button>
@@ -22,43 +39,39 @@
     </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            customer:{}
-        }
-    },
-    created:function() {
-        this.getEmployeeID();
-    },
-    methods:{
-        getEmployeeID(){
-            fetch('http://localhost:8000/api/customers/'+this.$route.params.id)
-            .then(response => response.json())
-            .then((dataResponse) => {
-                console.log(dataResponse);
-                this.customer = dataResponse.customer;
-            })
-            .catch(console.log)
-        },
-        editRegister(){
-            var data = {
-                id:this.$route.params.id,
-                name:this.customer.name,
-                email:this.customer.email
-            }
-            fetch('http://localhost:8000/api/customers/'+this.$route.params.id,{
-                method:"PUT",
-                body:JSON.stringify(data),
-                headers: { "Content-Type": "application/json" }
-            })
-            .then(response => response.json())
-            .then((dataResponse) => {
-                console.log(dataResponse);
-                window.location.href='/customers'
-            })
-        }
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import { showToastSuccess, handleErrors } from '@/utils/index';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const apiUrl = import.meta.env.VITE_API_URL;
+const customer = ref({});
+const formData = ref({});
+
+onMounted(() => {
+    getCustomer();
+});
+
+const getCustomer = async () => {
+    try {
+        const response = await axios.get(`${apiUrl}/customers/${route.params.id}`);
+        customer.value = response.data.customer;
+        formData.value = { ...customer.value }
+    } catch (error) {
+        handleErrors(error);
+    }
+};
+
+const updateCustomer = async () => {
+    try {
+        const response = await axios.put(`${apiUrl}/customers/${customer.value.id}`, formData.value); // Realiza una solicitud PUT a la API para actualizar el Customer
+        router.push('/customers');
+        showToastSuccess(response.data.message);
+    } catch (error) {
+        handleErrors(error);
     }
 }
 </script>
