b38684e9bbd5e20f20f0a888aadd3e4339ced0c6
Frans <frans_421_12@hotmail.com>
2023-10-08 23:12:07 -0500

    feat: login api

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="container mt-4">
-        <div class="card">
+        <div class="card my-6">
             <div class="card-body">
-                <pre>{{ user }}</pre>
                 <form @submit.prevent="login">
                     <div class="mb-3">
-                      <label for="email" class="form-label">Email</label>
-                      <input v-model="form.email" type="email" class="form-control" id="email" placeholder="Enter your email">
+                        <label for="email" class="form-label">Email</label>
+                        <input v-model="email" type="email" class="form-control" id="email" placeholder="Enter your email">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input v-model="form.password" type="password" class="form-control" id="password" placeholder="************">
+                        <input v-model="password" type="password" class="form-control" id="password" placeholder="************">
                     </div>
                     <button type="submit" class="btn btn-primary">Login</button>
                 </form>
@@ -19,33 +18,24 @@
     </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
 import axios from 'axios'
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8000';
+import router from '@/router';
+import { handleErrors } from '@/utils/index';
 
-export default {
-    data: () => ({
-        user: {},
-        form: {
-            email: 'frans@gmail.com',
-            password: 'password'
-        }
-    }),
-    methods: {
-        login() {
-            axios.get('/sanctum/csrf-cookie').then(() => {
-                axios.post("/login").then(res => {
-                    console.log(res);
-                });
-            });
-            
-            console.log("Enviando el login...", this.form);
-        }
+const apiUrl = import.meta.env.VITE_API_URL;
+const email = ref('');
+const password = ref('');
+
+const login = async () => {
+    try {
+        const response = await axios.post(`${apiUrl}/login`, { email: email.value, password: password.value });
+        const { accessToken, user } = response.data;
+
+        router.push('/customers');
+    } catch (error) {
+        handleErrors(error);
     }
-}
+};
 </script>
-
-<style lang="scss" scoped>
-
-</style>
